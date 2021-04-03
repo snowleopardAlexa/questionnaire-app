8ce2aabafed9711e920bdab8e7661a847ecd7e94
Alexa Slomski <alexaslomski@Alexas-MBP.home>
2021-04-02 22:37:26 -0400

    name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import FavoriteCuisine from './components/food/FavoriteCuisine';
 import RatePhotos from './components/photos/RatePhotos';
 
 function App() {
+  const [name] = useState("Questionnaire");
   return (
     <div className="App">
-      <h1>Questionnaire</h1>
+      <h1>{name}</h1>
       <FavoriteCuisine />
       <RatePhotos  totalStars={5} />
     </div>
